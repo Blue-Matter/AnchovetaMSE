1b60440898edb599134783e94100ec5704ee7a7b
tcarruth <tomsinbox@gmail.com>
2022-10-06 14:13:43 -0700

    change to splash

--- AnchovetaMSE/R/Plots.R
+++ AnchovetaMSE/R/Plots.R
@@ -1,6 +1,4 @@
-# Performance plot
-
-
+# Performance plots
 
 PA_TOplot<-function(MSEobj,refCMP=1,cex=0.9,xadj=6,yadj=1,MPcols=NULL){
 
@@ -84,6 +82,73 @@ TSplot<-function(MSEobj,slot="B",name="Biomass (Mt)",unit=1E9,MPcols=c("black","
   mtext("Year",1,line=0.7,outer=T)
 
 }
+
+# Robustness plots
+
+
+#  MSElist<-lapply(1:7,function(x)readRDS(paste0("MSE runs/MSE_",x,".rda")))
+
+PA_rob_plot<-function(MSElist,refMSE=1,MPnam="PA_HS_50",
+                      OMnams=c("2 Less Resil.","3 Time var. M","4 Inc. M","5 Dec. M",
+                               "6 Low Rec.","7 High Rec.")){
+
+  rMSE<-MSElist[[refMSE]]
+  MPind<-match(MPnam,rMSE@MPs)
+
+  Clong<-function(X,MSElist,rMSE){mean(MSElist[[X]]@Catch[,MPind,21:30]/rMSE@Catch[,MPind,21:30])*100}
+  Cshort<-function(X,MSElist,rMSE){mean(MSElist[[X]]@Catch[,MPind,1:10]/rMSE@Catch[,MPind,1:10])*100}
+  Blong<-function(X,MSElist,rMSE){mean(MSElist[[X]]@B[,MPind,21:30]/rMSE@B[,MPind,21:30])*100}
+
+  MSEind<-2:7
+
+  Cs<-sapply(MSEind,FUN=Cshort,MSElist=MSElist,rMSE=rMSE)
+  Cl<-sapply(MSEind,FUN=Clong,MSElist=MSElist,rMSE=rMSE)
+  Bl<-sapply(MSEind,FUN=Blong,MSElist=MSElist,rMSE=rMSE)
+
+  Clong5<-function(X,MSElist,rMSE){quantile(MSElist[[X]]@Catch[,MPind,21:30]/rMSE@Catch[,MPind,21:30],p=0.05)*100}
+  Cshort5<-function(X,MSElist,rMSE){quantile(MSElist[[X]]@Catch[,MPind,1:10]/rMSE@Catch[,MPind,1:10],p=0.05)*100}
+  Blong5<-function(X,MSElist,rMSE){quantile(MSElist[[X]]@B[,MPind,21:30]/rMSE@B[,MPind,21:30],p=0.05)*100}
+
+  Cs5<-sapply(MSEind,FUN=Cshort5,MSElist=MSElist,rMSE=rMSE)
+  Cl5<-sapply(MSEind,FUN=Clong5,MSElist=MSElist,rMSE=rMSE)
+  Bl5<-sapply(MSEind,FUN=Blong5,MSElist=MSElist,rMSE=rMSE)
+
+  ClongA<-function(X,MSElist,rMSE){as.vector(MSElist[[X]]@Catch[,MPind,21:30]/rMSE@Catch[,MPind,21:30])*100}
+  CshortA<-function(X,MSElist,rMSE){as.vector(MSElist[[X]]@Catch[,MPind,1:10]/rMSE@Catch[,MPind,1:10])*100}
+  BlongA<-function(X,MSElist,rMSE){as.vector(MSElist[[X]]@B[,MPind,21:30]/rMSE@B[,MPind,21:30])*100}
+
+  CsA<-sapply(MSEind,FUN=CshortA,MSElist=MSElist,rMSE=rMSE)
+  ClA<-sapply(MSEind,FUN=ClongA,MSElist=MSElist,rMSE=rMSE)
+  BlA<-sapply(MSEind,FUN=BlongA,MSElist=MSElist,rMSE=rMSE)
+
+  nMSEs<-length(MSElist)-1
+
+  nc=nMSEs
+  nr=3
+
+  par(mfcol=c(nr,nc),mai=c(0.3,0.3,0.01,0.01),omi=c(0.5,0.35,0.35,0.06))
+  ind<-1:length(MSElist)
+  ind<-ind[ind!=refMSE]
+  breaks<-seq(0,1000,by=2)
+  range(BlA,CsA,ClA)
+  xlim=c(85,115)
+  mline=2.8
+  for(i in 1:nMSEs){
+    hist(BlA[,i],breaks,main="",xlab="",ylab="",xlim=xlim)
+    abline(v=quantile(BlA,p=c(0.05,0.5,0.95)),col='#ff000080',lty=c(2,1,2))
+    mtext(OMnams[i],line=1.2,cex=0.8)
+    if(i ==1)mtext("Rel. Biomass, yrs 21-30",2,cex=0.8,line=mline)
+    hist(CsA[,i],breaks,main="",xlab="",ylab="",xlim=xlim)
+    abline(v=quantile(CsA,p=c(0.05,0.5,0.95)),col='#ff000080',lty=c(2,1,2))
+    if(i ==1)mtext("Rel. Catch, yrs 1-10",2,cex=0.8,line=mline)
+    hist(ClA[,i],breaks,main="",xlab="",ylab="",xlim=xlim)
+    abline(v=quantile(ClA,p=c(0.05,0.5,0.95)),col='#ff000080',lty=c(2,1,2))
+    if(i ==1)mtext("Rel. Catch, yrs 21-30",2,cex=0.8,line=mline)
+  }
+  mtext("Value Relative to Reference Case Result (%) by year-simulation",1,outer=T,line=1,cex=0.9)
+
+}
+
 
 
 
